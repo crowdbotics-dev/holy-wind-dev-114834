2c90f2090e18d1da5f973b55bc11fd937554e8ca
Crowdbotics <engineering@crowdbotics.com>
2023-09-07 23:18:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-wind-dev-114834",
+  url: "https://holy-wind-dev-114834.botics.co",
+  api: "https://holy-wind-dev-114834.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
